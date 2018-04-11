69b176024bfedfcbaa0437a07395dccb4addfb78
Paul Walsh <twentytwentyk@live.com>
2018-04-11 12:28:54 -0400

    caps

--- capitalize/index.js
+++ capitalize/index.js
@@ -7,6 +7,39 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+// //solution 1: easy to read, but requires good knowledge
+// function capitalize(str) {
+//   //Make an empty array 'words'
+//   let words = []
+//   //split the input string by spaces to get an array  
+//   for (let word of str.split(' ')) {
+//     //for each word in the array
+//     //uppercase the first letter of the word
+//     //join the first letter with the rest of the string (first indexslice starting at index 1)
+//     //push the result into 'words' array
+//     words.push(word[0].toUpperCase() + word.slice(1));
+//   }
+//   //join 'words' into a string and return it
+//   return words.join(' ');
+// }
 
+// solution 2: for loop, a litle more brutal to read.
+function capitalize(str) {
+  //create 'result' which is the first character of the input string capitalized
+  let result = str[0].toUpperCase();
+  //for each character in the string
+  for (let i = 1; i < str.length; i++) {
+    //if the character to the left is a space
+    if (str[i - 1] === ' ') {
+      //capitalize it and add it to result
+      result += str[i].toUpperCase();
+    } else {
+      //add it to result
+      result += str[i];
+    }
+  }
+  //dont forget to return
+  return result
+}
 module.exports = capitalize;
+ 
